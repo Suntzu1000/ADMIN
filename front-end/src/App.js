68525a2c044d5a8b192e4e2e12aa5d63be386d1e
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-01-19 15:00:10 -0300

    Página de Produtos completa

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -20,7 +20,7 @@ function App() {
           <Route element={<Layout/>}>
             <Route path="/" element={<Navigate to="dashboard" replace/>}/>
             <Route path="dashboard" element={<Dashboard/> }/>
-            <Route path="/products" element={<Products/>}/>
+            <Route path="/produtos" element={<Products/>}/>
           </Route>
         </Routes>
         </BrowserRouter>
--- front-end/src/scenes/products/Products.jsx
+++ front-end/src/scenes/products/Products.jsx
@@ -13,14 +13,117 @@ import {
 } from "@mui/material";
 import Header from "../../components/Header";
 import { useGetProductsQuery } from "../../state/api";
+
+const Product = ({
+  _id,
+  name,
+  description,
+  price,
+  rating,
+  category,
+  supply,
+  stat
+}) => {
+  const theme = useTheme()
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  return (
+    <Card 
+      sx={{
+        backgroundImage: "none",
+        backgroundColor: theme.palette.background.alt, 
+        borderRadius: "0.55rem"
+      }}
+      >
+        <CardContent>
+          <Typography sx={{fontSize: 14}} color={theme.palette.secondary[700]} gutterBottom>
+            {category}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography sx={{mb: "1.5rem" }} color={theme.palette.secondary[400]}>
+            ${Number(price).toFixed(2)}
+          </Typography>
+          <Rating value={rating} readOnly/>
+
+          <Typography variant="body2">{description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="primary"
+            size="small"
+            onClick={() => setIsExpanded(!isExpanded)}
+            >
+              Ver mais...
+          </Button>
+        </CardActions>
+        <Collapse 
+          in={isExpanded}
+          timeout="auto"
+          unmountOnExit
+          sx={{
+            color: theme.palette.neutral[400]
+          }}
+        >
+        <CardContent>
+          <Typography>id: {_id}</Typography>
+          <Typography>Fornecimento {supply}</Typography>
+          <Typography>Total Anual de Vendas:  {stat.yearlySalesTotal}</Typography>
+          <Typography>Total Anual de Unidades Vendidas: {stat.yearlyTotalSoldUnit}</Typography>
+          <Typography>id: {_id}</Typography>
+        </CardContent>
+        </Collapse>
+    </Card>
+  )
+}
+
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
-  console.log("data", data);
+  
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="PRODUTOS" subtitle="Veja sua lista de produtos." />
-      {data || !isLoading ? <Box mt="20px" ></Box> : <></>}
+      {data || !isLoading ? (
+        <Box
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            "& > div": {gridColumn: isNonMobile ? undefined : "span 4"}
+          }}
+        >
+         {data.map( ({
+            _id,
+            name,
+            description,
+            price,
+            rating,
+            category,
+            supply,
+            stat
+         }) => (
+          < Product
+          key={_id}
+          _id={_id}
+          name={name}
+          description={description}
+          price={price}
+          rating={rating}
+          category={category}
+          supply={supply}
+          stat={stat}
+            >
+          </Product>
+         ))}
+        </Box>
+      ) : (
+        <>Carregando...</>
+      )}
     </Box>
   );
 };
