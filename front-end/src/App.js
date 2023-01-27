401512d7b1e4d72ad816679076c2f89652ffead9
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-01-27 09:45:05 -0300

    Admin

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,6 +14,7 @@ import Overview from "./scenes/overview/Overview"
 import Daily from "./scenes/daily/Daily"
 import Mensalmente from "./scenes/monthly/Mensalmente";
 import PaginaDetalhamento from "./scenes/breakdown/PaginaDetalhamento";
+import Admin from "./scenes/admin/Admin"
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -35,6 +36,7 @@ function App() {
             <Route path="/diariamente" element={<Daily/>}/>
             <Route path="/mensalmente" element={<Mensalmente/>}/>
             <Route path="/página de detalhamento" element={<PaginaDetalhamento/>}/>
+            <Route path="/admin" elemennt={<Admin/>}/>
           </Route>
         </Routes>
         </BrowserRouter>
